4efdf34d913497a03934a5704a1e5470c96dc71a
sovka185 <solecka.sofie@gmail.com>
2021-11-16 21:38:20 +0000

    simple wall post

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ app.use(express.static("client"));
 app.use(bodyParser.json());
 
 var data = require("./data.json");
+var data = require("./schools.json");
 
 app.listen(888);
 
-app.get("", function (req, resp){
-    
+app.post("/wallColor", function (req, resp){
+    let wallColor = req.body;
+
+    let id = wallColor.id; //for now, probs gonna use express-session
+    let colorHex = wallColor.colorHex;
+
+    for(let d of data){
+        if(d.user == id){
+            d.wall = colorHex;
+        }
+    }
+    resp.json("done");
+
 });
