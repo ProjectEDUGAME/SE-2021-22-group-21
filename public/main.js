cc955b9ee6302b375c93814404355d3ba39a3132
mingyeahh <mingyeahh@users.noreply.github.com>
2022-03-01 11:12:59 +0000

    delete duplicate html files

--- public/main.js
+++ public/main.js
@@ -8,35 +8,35 @@ const switchTo = (currentID, nextID) => {
     document.getElementById(nextID).classList.remove("hidden");
 };
 
-const accessStringSubmit = (event) => {
-    event.preventDefault();
+// const accessStringSubmit = (event) => {
+//     event.preventDefault();
 
-    let data = {};
+//     let data = {};
 
-    let accessStr = document.getElementById('child-str').value;
-    if (accessStr === ''){
-        console.log('Please put in a valid institue log in string.');
-        return;
-    }
+//     let accessStr = document.getElementById('child-str').value;
+//     if (accessStr === ''){
+//         console.log('Please put in a valid institue log in string.');
+//         return;
+//     }
 
-    data['access-str'] = accessStr;
+//     data['access-str'] = accessStr;
 
-    fetch('/login', {
-        method: 'POST',
-        body: data,
-    }).then((res) =>{
-        if (res.ok) {
-            return res.json();
-        } else {
-            throw new Error(res.status);
-        }
-    }).then((resData) => {
-        // {"school-name": "St Mary's Primary School"}
-        let schoolName = resData['school-name'];
-        document.getElementById('school-name').innerText = schoolName;
-        switchTo('child-log-in', 'participant-details');
-    }).catch((err) =>{
-        console.error(err);
-    });
-}
+//     fetch('/login', {
+//         method: 'POST',
+//         body: data,
+//     }).then((res) =>{
+//         if (res.ok) {
+//             return res.json();
+//         } else {
+//             throw new Error(res.status);
+//         }
+//     }).then((resData) => {
+//         // {"school-name": "St Mary's Primary School"}
+//         let schoolName = resData['school-name'];
+//         document.getElementById('school-name').innerText = schoolName;
+//         switchTo('child-log-in', 'participant-details');
+//     }).catch((err) =>{
+//         console.error(err);
+//     });
+// }
 
