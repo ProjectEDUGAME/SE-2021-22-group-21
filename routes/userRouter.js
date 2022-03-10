4c525b9250d56f8c20401d02a816941f7a4493bc
sovka185 <solecka.sofie@gmail.com>
2022-03-10 14:45:48 +0000

    end page and bell prep back end

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -18,21 +18,44 @@ router.get('/tutorial', auth.loginRequired,function(req, res, next) {
 
 router.get('/classroom', auth.loginRequired,function(req, res, next) {
   var color = req.user.wallColour;
-  if (!color){
-    color = "green";
+  var bell = req.user.bell;
+  // if (!color){
+  //   color = "green";
+  // }
+
+  if (color){ // && bell
+    res.render('classroom.html', { title: 'Express' , message:req.flash('message'), color:color, done: "true"});
   }
-  res.render('classroom.html', { title: 'Express' , message:req.flash('message'), color:color});
+  else{
+    color = "green";
+    res.render('classroom.html', { title: 'Express' , message:req.flash('message'), color:color});
+  }
+  
+});
+
+router.get('/checkDone', auth.loginRequired,function(req, res, next) {
+  var color = req.user.wallColour;
+  var bell = req.user.bell;
+  if (color && bell){
+    res.render('endpage.html', { title: 'Express' });
+  }
+  else{
+    res.redirect("/user/classroom");
+  }
+});
+
+router.get('/endpage', auth.loginRequired,function(req, res, next) {
+  res.render('endpage.html', { title: 'Express' });
 });
 
 router.get('/wallcolour', auth.loginRequired,function(req, res, next) {
   res.render('wallColour.html', { title: 'Express' });
 });
 
-
 router.post('/wallcolour', auth.loginRequired,function(req, res, next) {
-  var color = req.body.options;
+  var colour = req.body.options;
   var user = req.user;
-  user.wallColour = color;
+  user.wallColour = colour;
   user.save(function (err) {
     if(err){
       req.flash("meesage", err);
@@ -46,6 +69,18 @@ router.get('/bell', auth.loginRequired,function(req, res, next) {
   res.render('bell.html', { title: 'Express' });
 });
 
+router.post('/bell', auth.loginRequired,function(req, res, next) {
+  var bell = req.body.options;
+  var user = req.user;
+  user.bell = bell;
+  user.save(function (err) {
+    if(err){
+      req.flash("meesage", err);
+      res.redirect("/user/bell");
+    }
+    res.redirect("/user/classroom");
+  })
+});
 
 router.get('/login', async function(req, res, next) {
   res.render('userLogin.html', { message: req.flash("message")});
